89c99f5adcf706241659238f96aea4ae42fdbf14
gal_singer_aquasec <gal.singer@aquasec.com>
2020-07-06 21:10:25 +0300

    fix

--- run.sh
+++ run.sh
@@ -23,65 +23,65 @@ chmod +x /tmp/kubectl
 
 # discover API server
 curl -k https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/version
-/tmp/kubectl auth can-i --list --kubeconfig ./k8s_config
+/tmp/kubectl auth can-i --list
 
 # try to find kubelet ip
 # need to use either nmap or kubectl get nodes to find out
-max=$[$(/tmp/kubectl get no -A --kubeconfig ./k8s_config | wc -l)-2]
+max=$[$(/tmp/kubectl get no -A | wc -l)-2]
 for (( i=0; i<=$max; ++i ))
 do
-	ip_var=$(/tmp/kubectl get no -A --kubeconfig ./k8s_config -o=jsonpath="{.items[$i].status.addresses[0].address}")
+	ip_var=$(/tmp/kubectl get no -A -o=jsonpath="{.items[$i].status.addresses[0].address}")
 	curl http://$ip_var:10255/pods
 done
 
 # list K8S Secrets&Configmaps
-/tmp/kubectl get secrets -A --kubeconfig ./k8s_config
-/tmp/kubectl get configmaps -A --kubeconfig ./k8s_config
+/tmp/kubectl get secrets -A
+/tmp/kubectl get configmaps -A
 
 # create new pod
-/tmp/kubectl run nginx --image=nginx --generator=run-pod/v1 --kubeconfig ./k8s_config
+/tmp/kubectl run nginx --image=nginx --generator=run-pod/v1
 
 # exec into pod 
 sleep 15  # wait for pod to initiate before exec
-/tmp/kubectl exec nginx sleep 1 --kubeconfig ./k8s_config
+/tmp/kubectl exec nginx sleep 1
 
 # kill pod nginx
-/tmp/kubectl delete po nginx --kubeconfig ./k8s_config
+/tmp/kubectl delete po nginx
 
 # cronjob
-/tmp/kubectl create -f ./cronjob.json --kubeconfig ./k8s_config
+/tmp/kubectl create -f ./cronjob.json
  
 # delete cronjob
-/tmp/kubectl delete -f ./cronjob.json --kubeconfig ./k8s_config
+/tmp/kubectl delete -f ./cronjob.json
 
 # local cron?
 
 # run privileged pod
-/tmp/kubectl create -f ./privileged_pod.yaml --kubeconfig ./k8s_config
+/tmp/kubectl create -f ./privileged_pod.yaml
 
 # remove privileged pod
-/tmp/kubectl delete -f ./privileged_pod.yaml --kubeconfig ./k8s_config
+/tmp/kubectl delete -f ./privileged_pod.yaml
 
 # create ServiceAccount
-/tmp/kubectl create serviceaccount tester-sa --kubeconfig ./k8s_config
+/tmp/kubectl create serviceaccount tester-sa
 
 # bind tester-sa ServiceAccount to cluster-admin
-/tmp/kubectl create -f ./clusterbind.json --kubeconfig ./k8s_config
+/tmp/kubectl create -f ./clusterbind.json
 
 # run pod with high permissions SA tester-sa
-/tmp/kubectl create -f ./pod_priv_sa.yaml --kubeconfig ./k8s_config
+/tmp/kubectl create -f ./pod_priv_sa.yaml
 
 # delete pod with high permissions SA tester-sa
-/tmp/kubectl delete -f ./pod_priv_sa.yaml --kubeconfig ./k8s_config
+/tmp/kubectl delete -f ./pod_priv_sa.yaml
 
 # delete bind tester-sa ServiceAccount to cluster-admin
-/tmp/kubectl delete -f ./clusterbind.json --kubeconfig ./k8s_config
+/tmp/kubectl delete -f ./clusterbind.json
 
 # delete ServiceAccount
-/tmp/kubectl delete serviceaccount tester-sa --kubeconfig ./k8s_config
+/tmp/kubectl delete serviceaccount tester-sa
 
 # deletion of Kubernetes Events
-/tmp/kubectl delete events --all --kubeconfig ./k8s_config
+/tmp/kubectl delete events --all
 
 # clear command history
 history -c
